fc4224fa5c0835a76abd3d7d93ddc478af333021
Kai Banto Eckert <y.banto@outlook.com>
2020-08-21 16:00:19 +0200

    Minimal code

--- csapp/views.py
+++ csapp/views.py
@@ -1,18 +1,9 @@
-import time
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/server/time')
-def get_current_time():
-    return {'time': time.time()}
-
-
-if __name__ == '__main__':
-    pass
