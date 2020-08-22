d9f6f1b2ce906a7d0408d1090dea79047cccb49d
Kai Banto Eckert <y.banto@outlook.com>
2020-08-23 00:17:48 +0200

    Created a CLI command to create IndexItem Table in SQLite

--- csapp/__init__.py
+++ csapp/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
-
+# import sqlite3
+from . import models
 from .views import app
+from . import utils
 
+
+@app.cli.command()
+def create_indexitem_table():
+    """Create IndexItem Table."""
+    utils.create_indexitem_table()
