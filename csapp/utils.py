dc2832261ab7b13594e5c33ad8339d7f6d17925f
Kai Banto Eckert <y.banto@outlook.com>
2020-08-23 00:14:01 +0200

    Added write_html() function to create HTML files

---
+++ csapp/utils.py
@@ -0,0 +1,66 @@
+if __name__ != "__main__":
+    from csapp import models
+import sqlite3
+
+
+def dict_factory(cursor, row):
+    return dict([(col[0], row[idx]) for idx, col in enumerate(cursor.description)])
+
+
+def create_indexitem_table():
+    # Start connection with the database (also creates the file if it does not yet exist)
+    con: sqlite3.Connection = sqlite3.connect(app.config['DATABASE_URI'])
+
+    # Using the nonstandard `execute()` method of the `Connection` object, we don't have to create the `Cursor` objects explicitly
+    con.execute("""CREATE TABLE IF NOT EXISTS `indexitem` 
+                   (id INTEGER PRIMARY KEY ASC, label VARCHAR NOT NULL UNIQUE);""")
+    
+    con.commit()
+    con.close()
+
+
+def write_html(label: str):
+    if not isinstance(label, str):
+        raise TypeError(f"'str' expected, not {type(label).__name__!r}.")
+
+    capitalized_label: str = label.capitalize()
+
+    return f"""{{% extends 'base.html' %}}
+
+{{% block head %}}
+    <link rel="stylesheet" href="{{{{ url_for('static', filename='css/cheatsheet.css', _external=True) }}}}">
+    <title>{capitalized_label} Reminder</title>
+{{% endblock %}}
+
+{{% block body %}}
+    <div class="main-container">
+
+        <header>
+            <div class="header-container">
+                <nav>
+                    <div>
+                        <ul class="nav-list">
+                        <li><a id="home" href="{{{{ url_for('index', _external=True) }}}}" >Back to Index</a></li>
+                        </ul>
+                    </div>
+                </nav>
+                <div class="main-title">
+                    <h1>{capitalized_label}</h1>
+                </div>
+            </div>
+        </header>
+
+        <section>
+            <aside></aside>
+            <article></article>
+            <aside></aside>
+        </section>
+
+        <footer></footer>
+
+    </div>
+{{% endblock %}}"""
+
+
+if __name__ == "__main__":
+    print(write_html('pandas'))
