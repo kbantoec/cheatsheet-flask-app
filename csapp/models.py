0ff17521e382989712d49d4264d7a26f67d29a9e
Kai Banto Eckert <y.banto@outlook.com>
2020-08-23 00:21:37 +0200

    Added some dynamically defined attributes

--- csapp/models.py
+++ csapp/models.py
@@ -1,75 +1,31 @@
-from flask_sqlalchemy import SQLAlchemy
-from .views import app
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'app.db')}"
-
-# Start connection with the database
-db = SQLAlchemy(app)
-
-"""
->>> from csapp.models import db
->>> from csapp.models import CheatSheetContent
->>> db.create_all()
->>> CheatSheetContent.query.all()
-
-"""
+import sqlite3
+if __name__ != "__main__":
+    from .views import app
 
 
-class CheatSheetContent(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    h1 = db.Column(db.String(200), nullable=False)  # This title cannot be empty
-    h2 = db.Column(db.String(200))
-    cell1 = db.Column(db.Text)
-    cell2 = db.Column(db.Text)
-    syntax = db.Column(db.Text)
-    example_content = db.Column(db.Text)
-    example_caption = db.Column(db.Text)
-    method = db.Column(db.Text)
-    method_description = db.Column(db.Text)
-    link = db.Column(db.Text)
-    link_href = db.Column(db.Text)
-    link_type = db.Column(db.String(10), default='code')
+class IndexItem:
 
-    def __init__(self, h1, h2, cell1, cell2, syntax, example_content, example_caption, method, method_description, link,
-                 link_href, link_type):
-        self.h1 = h1
-        self.h2 = h2
-        self.cell1 = cell1
-        self.cell2 = cell2
-        self.syntax = syntax
-        self.example_content = example_content
-        self.example_caption = example_caption
-        self.method = method
-        self.method_description = method_description
-        self.link = link
-        self.link_href = link_href
-        self.link_type = link_type
+    def __init__(self, label: str, id: int = None):
+        # Check if all arguments are right
+        if not isinstance(label, str):
+            raise TypeError(f"'label' should be a 'str', not {type(label)!r}.")
 
+        if (not isinstance(id, int)) and (id is not None):
+            raise TypeError(f"'id' should be an 'int' or 'None', not {type(id)!r}.")
+        
+        # Do assignments
+        self.label: str = label
+        self.lower_label: str = label.lower()
+        self.filename: str = f"{label.lower()}.html"
+        self.id = id
+    
+    def __str__(self):
+        return f"{self.__dict__}"
+    
     def __repr__(self):
-        return f'CheatSheetContent {self.id}'
+        return f"<IndexItem {self.id!r}>"
 
 
-def init_db():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
-
-
-if __name__ == '__main__':
-    db.create_all()
-    db.session.add(CheatSheetContent(h1="pandas.Series",
-                   h2="",
-                   cell1="pandas.Series.value_counts",
-                   cell2="Return a Series containing counts of unique values.",
-                   syntax="Series.value_counts(self, normalize=False, sort=True, ascending=False, bins=None, dropna=True)",
-                   example_content="",
-                   example_caption="",
-                   method="",
-                   method_description="",
-                   link="Exercise 2.4.2: How thresholds affect performance, Credit Risk Modeling in Python (Datacamp), Chapter 2: Logistic Regression for Defaults, Section 2.4: Model discrimination and impact",
-                   link_href="C:/Users/YBant/Documents/projects/my_wiki/content/datacamp/credit_risk_modeling/chapter2.html#how_thresholds_affect_performance",
-                   link_type="code"))
-    db.session.commit()
-
+if __name__ == "__main__":
+    item1 = IndexItem('pandas')
+    print(item1)
