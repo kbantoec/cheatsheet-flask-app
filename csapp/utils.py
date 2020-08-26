5374a46378758a28d98561cf37ed0740c3af5b21
Kai Banto Eckert <y.banto@outlook.com>
2020-08-26 17:55:38 +0200

    Added create_reminder_table function

--- csapp/utils.py
+++ csapp/utils.py
@@ -7,7 +7,7 @@ def dict_factory(cursor, row):
     return dict([(col[0], row[idx]) for idx, col in enumerate(cursor.description)])
 
 
-def create_indexitem_table():
+def create_indexitem_table(app):
     # Start connection with the database (also creates the file if it does not yet exist)
     con: sqlite3.Connection = sqlite3.connect(app.config['DATABASE_URI'])
 
@@ -15,6 +15,14 @@ def create_indexitem_table():
     con.execute("""CREATE TABLE IF NOT EXISTS `indexitem` 
                    (id INTEGER PRIMARY KEY ASC, label VARCHAR NOT NULL UNIQUE);""")
     
+    con.commit()
+    con.close()
+
+
+def create_reminder_table(app):
+    con: sqlite3.Connection = sqlite3.connect(app.config['DATABASE_URI'])
+    con.execute("""CREATE TABLE IF NOT EXISTS `reminder` 
+                   (id INTEGER PRIMARY KEY ASC, label VARCHAR, h1 TEXT, content_cell_1 TEXT, content_cell_2 TEXT);""")
     con.commit()
     con.close()
 
