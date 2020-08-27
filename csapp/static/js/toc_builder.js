7dba7785a324c594e4179401558354f5bc8949fb
Kai Banto Eckert <y.banto@outlook.com>
2020-08-27 17:43:44 +0200

    Created getBaseUri() function, modal buttons, TOC links with href attribute, and added some documentation comments.

--- csapp/static/js/toc_builder.js
+++ csapp/static/js/toc_builder.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Creates the table of contents (TOC) dynamically
+    // Needs:
+    // - getBaseUri(): to set the href attribute of the a tags in the TOC
     buildToc();
+
+    // Re-interprets the injected database content as HTML and adds the modal butons
     reinterpretTextAsHTML();
 });
 
@@ -12,8 +17,16 @@ const buildToc = () => {
 
     // Add the items to the list
     for (let i = 0; i < tocItems.length; i++) {
+        const IthtocItem = 'toc-item-' + i;
+        tocItems[i].setAttribute('id', IthtocItem);
+
+        const a = document.createElement('a');
+        a.setAttribute('href', getBaseUri() + '#' + IthtocItem);
+        a.innerHTML = tocItems[i].innerText;
+        
+
         const li = document.createElement('li');
-        li.innerHTML = tocItems[i].innerText;
+        li.append(a);
         ol.append(li);
     }
 };
@@ -27,7 +40,30 @@ const reinterpretTextAsHTML = () => {
         tocItems[i].innerHTML = tocItems[i].innerText;
     }
 
-    for (let i = 0; i < tds.length; i++) {
-        tds[i].innerHTML = tds[i].innerText;
+    // for (let i = 0; i < tds.length; i++) {
+    //     tds[i].innerHTML = tds[i].innerText;
+    //     if (i % 2 !== 0) {
+    //         addModalBtn(tds[i]);
+    //     }
+    // }
+
+    for (let i = 0; i < tds.length; i++) {  
+        tds[i].innerHTML = tds[i].innerText;      
+        if (i % 2 !== 0) {
+            // Appends the modal button if it is a cell of the second column in the table
+            const btn = `<button data-modal-target="#modal" class="modalBtn"><span><i class="fa fa-info-circle"></i></span></button>`;
+            tds[i].innerHTML += btn;
+        }
     }
 };
+
+
+const getBaseUri = () => {
+    return document.getElementsByClassName('main-title')[0].firstChild["baseURI"];
+};
+
+
+// const addModalBtn = (element) => {
+//     const btn = `<button data-modal-target="#modal" class="modalBtn"><span><i class="fa fa-info-circle"></i></span></button>`;
+//     element.innerHTML = element.innerText + "&nbsp;" + btn;
+// };
