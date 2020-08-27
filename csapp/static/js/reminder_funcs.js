5325882dd17ca82e89c1f1e367331a63f5894988
Kai Banto Eckert <y.banto@outlook.com>
2020-08-27 17:49:38 +0200

    Changed the add-item-btn icon

--- csapp/static/js/reminder_funcs.js
+++ csapp/static/js/reminder_funcs.js
@@ -1,15 +1,13 @@
-// const addItemToTable = () => {
-//     const btn = document.getElementById("add-item-to-table");
-//     return ((btn.style.display === "block") ? btn.style.display = "none" : btn.style.display = "block");
-// }
-
 const addItemToTable = (element) => {
     const containerDiv = element.parentElement.parentElement;
     const forms = containerDiv.getElementsByTagName('form');
+    console.log(element.innerText);
     if (forms.length > 0) {
-        return ((forms[0].style.display === "block") ? forms[0].style.display = "none" : forms[0].style.display = "block");
+        const instruction1 = (forms[0].style.display === "block") ? forms[0].style.display = "none" : forms[0].style.display = "block";
+        const instruction2 = (element.innerHTML === `<i class="fa fa-plus-square"></i>`) ? element.innerHTML = `<i class="fa fa-minus-square"></i>` : element.innerHTML = `<i class="fa fa-plus-square"></i>`;
+        return (instruction1) && (instruction2);
     }
-}
+};
 
 
 const autoGrow = (element) => {
@@ -17,4 +15,4 @@ const autoGrow = (element) => {
     const bodyFontSize = window.getComputedStyle(body, null).getPropertyValue("font-size");
     element.style.height = bodyFontSize;
     element.style.height = (element.scrollHeight) + "px";
-}
+};
