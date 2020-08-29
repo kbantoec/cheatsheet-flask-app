7c622033cb8a9405b3e411d2722679ca143b3cec
Kai Banto Eckert <y.banto@outlook.com>
2020-08-29 11:08:58 +0200

    Modifier Reminder class and created Example class

--- csapp/models.py
+++ csapp/models.py
@@ -27,15 +27,40 @@ class IndexItem:
 
 
 class Reminder:
-    def __init__(self, label: str = None, h1: str = None, content_cell_1: str = None, content_cell_2: str = None, id: int = None):
+    def __init__(self, label: str = None, 
+                 h1: str = None, 
+                 content_cell_1: str = None, 
+                 content_cell_2: str = None, 
+                 lang: str = None,
+                 syntax_content: str = None,
+                 example_content: str = None,
+                 example_caption: str = None,
+                 link_text: str = None,
+                 link_href: str = None,
+                 link_type: str = None,
+                 id: int = None):
         self.label: str = label.lower()
         self.h1: str = h1
         self.content_cell_1: str = content_cell_1
         self.content_cell_2: str = content_cell_2
-        self.id = id
+        self.lang: str = "py" if lang is (None or "") else lang
+        self.syntax_content: str = syntax_content
+        self.example_content: str = example_content
+        self.example_caption: str = example_caption
+        self.link_text: str = link_text
+        self.link_href: str = link_href
+        self.link_type: str = "code" if link_type is (None or "") else link_type
+        self.id: int = id
 
     def __str__(self):
         return f"{self.__dict__}"
+    
+
+class Example:
+    def __init__(self, caption: str = None, content: str = None, id: int = None):
+        self.caption: str = caption
+        self.content: str = content
+        self.id: int = id
 
 
 if __name__ == "__main__":
