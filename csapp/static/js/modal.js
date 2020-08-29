39f02a5f5ce79b4e4fb59d20f8415650d6d13beb
Kai Banto Eckert <y.banto@outlook.com>
2020-08-29 11:11:00 +0200

    Created createSyntaxFrom(), createExampleFrom(), and createLinksListFrom() [not finished] functions and added them to the addEventListener of DOMContentLoaded

--- csapp/static/js/modal.js
+++ csapp/static/js/modal.js
@@ -4,17 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const openModalButtons = document.querySelectorAll('[data-modal-target]');
     const closeModalButtons = document.querySelectorAll('[data-close-button]');
     const overlay = document.getElementById('overlay');
+
     openModalButtons.forEach(button => {
         button.addEventListener('click', () => {
             // Use the data attribute from the button with button.dataset
             const modal = document.querySelector(button.dataset.modalTarget);
             openModal(modal);
-            // let modalBody = modal.lastElementChild;
+            const modalBody = modal.lastElementChild;
             
     
-            // createSyntaxFrom(button, modalBody);
-            // createExamplesFrom(button, modalBody);
-            // createLinksListFrom(button, modalBody);
+            createSyntaxFrom(button, modalBody);
+            createExampleFrom(button, modalBody);
+            createLinksListFrom(button, modalBody);
             // modalBody.setAttribute('onload', 'hljs.initHighlightingOnLoad()');
         });
     });
@@ -43,6 +44,7 @@ const openModal = (modal) => {
     }
 };
 
+
 const closeModal = (modal) => {
     if (modal != null) {
         let modalBody = modal.lastElementChild;
@@ -54,6 +56,62 @@ const closeModal = (modal) => {
 };
 
 
+
+const createSyntaxFrom = (button, element) => {
+    // let tr = document.querySelectorAll('[data-modal-target]')[0].parentElement.parentElement;
+    const tr = button.parentElement.parentElement;
+
+    if (tr.dataset.syntaxContent.length > 0) {
+        const lang = tr.dataset.lang;
+        const syntaxContent = tr.dataset.syntaxContent;
+
+        const h1 = document.createElement('h1');
+        h1.innerText = "Syntax";
+
+        const pre = document.createElement('pre');
+        // pre.classList.add('lil');
+
+        const code = document.createElement('code');
+        code.classList.add(lang);
+        code.innerText = syntaxContent;
+
+        pre.append(code);
+        element.append(h1);
+        element.append(pre);
+    }    
+};
+
+const createExampleFrom = (button, element) => {
+    const tr = button.parentElement.parentElement;
+
+    if (tr.dataset.exampleContent.length > 0) {
+        const lang = tr.dataset.lang;
+        const exampleContent = tr.dataset.exampleContent;
+        const exampleCaption = tr.dataset.exampleCaption;
+
+        const h1 = document.createElement('h1');
+        h1.innerText = "Examples";
+
+        const h2 = document.createElement('h2');
+        h2.innerHTML = exampleCaption;
+
+        const pre = document.createElement('pre');
+        // pre.classList.add('lil');
+
+        const code = document.createElement('code');
+        code.classList.add(lang);
+        code.innerText = exampleContent;
+
+        pre.append(code);
+        element.append(h1);
+        element.append(h2);
+        element.append(pre);
+    }    
+};
+
+const createLinksListFrom = () => {
+
+};
 
 
 
