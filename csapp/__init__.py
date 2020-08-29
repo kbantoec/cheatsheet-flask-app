5c497876cd7e52d879594533321d866f5aca5f13
Kai Banto Eckert <y.banto@outlook.com>
2020-08-29 11:08:11 +0200

    Created disable-fk, enable-fk, init-database, drop-reminder-table-data, and drop-reminder-table CLI commands.

--- csapp/__init__.py
+++ csapp/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-# import sqlite3
 from . import models
 from .views import app
 from . import utils
@@ -15,3 +14,36 @@ def create_indexitem_table():
 def create_reminder_table():
     """Create Reminder Table."""
     utils.create_reminder_table(app)
+
+
+@app.cli.command()
+def drop_reminder_table():
+    """Drop Reminder Table."""
+    utils.drop_reminder_table(app)
+
+@app.cli.command()
+def drop_reminder_table_data():
+    """Drop data inside the Reminder Table."""
+    utils.drop_reminder_table_data(app)
+
+
+@app.cli.command()
+def init_database():
+    """Initialize the database."""
+    utils.create_commands_table(app)
+    utils.create_examples_table(app)
+    utils.create_links_table(app)
+    utils.create_commands_examples_table(app)
+    utils.create_commands_links_table(app)
+
+
+@app.cli.command()
+def enable_fk():
+    """Enable foreign key constraint."""
+    utils.enable_fk(app)
+
+
+@app.cli.command()
+def disable_fk():
+    """Disable foreign key constraint."""
+    utils.disable_fk(app)
